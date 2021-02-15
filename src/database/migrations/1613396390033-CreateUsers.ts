d09ceaa7e84e3a67b62c2f4e1a88e6e87f80ca08
Diego Bonagurio <diego.bonagurio@gmail.com>
2021-02-15 15:13:37 -0300

    tabela users

---
+++ src/database/migrations/1613396390033-CreateUsers.ts
@@ -0,0 +1,58 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateUsers1613396390033 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+			new Table({
+				name: 'users', 
+				columns: [
+					{
+						name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+					},
+					{
+						name: 'name',
+						type: 'varchar',
+						isNullable: true,
+					},
+					{
+						name: 'email',
+						type: 'varchar',
+						isUnique: true,
+					},
+					{
+						name: 'password',
+						type: 'varchar',
+					},
+					{
+						name: 'cpf',
+						type: 'int',
+					},
+					{
+						name: 'deliveryman',
+						type: 'boolean',
+					},
+					{
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+				]
+			})
+		);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users')
+    }
+}
+
+
